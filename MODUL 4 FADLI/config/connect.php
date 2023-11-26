4cffbbb75b7eab7d8c96afaac21bca2c80baf6da
FADLI KURNIAWAN <aku.fadli.kurniawan@gmail.com>
2023-11-26 13:44:35 +0700

    mantap

---
+++ MODUL 4 FADLI/config/connect.php
@@ -0,0 +1,18 @@
+<!-- File ini berisi koneksi dengan database MySQL -->
+<?php 
+
+// (1) Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$conn = mysqli_connect("localhost","root","","modul4_wad");
+
+  
+
+
+// 
+
+// (2) Buatlah perkondisian untuk menampilkan pesan error ketika database gagal terkoneksi
+if(!$conn){
+    echo "error" . mysqli_connect_error();
+}
+// 
+ 
+?>
